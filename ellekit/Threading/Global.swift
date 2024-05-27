77bd51f510d34228006d625bc2b98ef00bb5919e
c 👒 <charlotte@macbook-air-3.home>
2024-05-27 14:12:12 -0400

    Fix getAllThreads

--- ellekit/Threading/Global.swift
+++ ellekit/Threading/Global.swift
@@ -7,7 +7,9 @@ import Foundation
 func getAllThreads() -> [thread_act_t] {
     let act_list: UnsafeMutablePointer<thread_act_array_t?> = .allocate(capacity: 100)
     var count: UInt32 = 0
-    task_threads(mach_task_self_, act_list, &count)
+    guard task_threads(mach_task_self_, act_list, &count) == KERN_SUCCESS else {
+        return []
+    }
     let threadArray = act_list.pointee?.withMemoryRebound(to: thread_act_t.self, capacity: MemoryLayout<thread_act_t>.size * Int(count), { ptr in
         Array(UnsafeMutableBufferPointer(start: ptr, count: Int(count)))
     })
